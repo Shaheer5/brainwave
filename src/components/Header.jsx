55890388bac17b3f7bf746cb23e899354e662488
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-03-27 18:15:52 +0500

    starter mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,35 @@ import { useLocation } from "react-router-dom";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
   const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(true);
+
+  const toggleNavigation = () => {
+    if(openNavigation) {
+      setOpenNavigation(false);
+    } else {
+      setOpenNavigation(true);
+    }
+  }
+
+  const handleClick = () => {
+    if(openNavigation) {
+      setOpenNavigation(false);
+    }
+  }
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b
-    border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b
+    border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8' : 'bg-n-8/90 backdrop-blur-sm'}`}>
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:pt-4">
         <a href="#hero" className="block w-[12rem] xl:mr-8">
           <img src={brainwave} alt="" width={190} height={40} />
         </a>
-        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+        <nav className={`${openNavigation ? "flex" : "hidden"} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map(item => (
               <a href={item.url} key={item.id} className={`block relative
@@ -25,6 +43,7 @@ const Header = () => {
                 {item.title}
               </a>
             ))}
+            <HamburgerMenu />
           </div>
         </nav>
         <a
@@ -35,6 +54,9 @@ const Header = () => {
         </a>
         <Button className="hidden lg:flex" href="#login">
           Sign in
+        </Button>
+        <Button className="ml-auto lg:hidden px-3" onClick={toggleNavigation}>
+          <MenuSvg openNavigation={openNavigation}/>
         </Button>
       </div>
     </div>
