64c8c7b06534d1dd4458e05aad21f2804ca185c8
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-03-27 09:09:02 +0500

    starter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,10 +12,14 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'no-warning-comments': 'off',
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    'react/prop-types': 'off',
+    'react-refresh/only-export-components': 'off',
   },
-}
+};
