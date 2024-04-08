63112feedbf8775f61617fee9d348f8dda4dd808
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-04-08 20:15:39 +0500

    Pricing Section Completed

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -3,6 +3,7 @@ import Section from "./Section";
 import { smallSphere, stars } from "../assets";
 import Heading from "./Heading";
 import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
@@ -27,13 +28,24 @@ const Pricing = () => {
           </div>
         </div>
         <Heading
-          className="text-center"
           tag="Get Started with Brainwave"
           title="Pay once, Use Forever"
         />
-        <div className="relatve">
+        <div className="relative">
           <PricingList />
+          <LeftLine />
+          <RightLine />
         </div>
+
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
+        </div>
+        
       </div>
     </Section>
   );
