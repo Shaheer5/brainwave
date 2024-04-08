a29c8654fa8695e142fb95c14396448ebd99b710
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-04-08 21:16:54 +0500

    design update

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import Section from "./Section";
-import Heading from "./Heading";
 import { benefits } from "../constants";
+import Heading from "./Heading";
+import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
-    <Section id={"features"}>
+    <Section id="features">
       <div className="container relative z-2">
         <Heading
-          className={"md:max-w-md lg:max-w-2xl text-center"}
-          title={"Chat Smarter, Not Harder with Brainwave"}
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
         />
+
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              key={item.id}
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
+              key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5 className="h5 mb-5">{item.title}</h5>
@@ -39,6 +39,7 @@ const Benefits = () => {
                   <Arrow />
                 </div>
               </div>
+
               {item.light && <GradientLight />}
 
               <div
@@ -51,12 +52,13 @@ const Benefits = () => {
                       src={item.imageUrl}
                       width={380}
                       height={362}
-                      alt="item-title"
+                      alt={item.title}
                       className="w-full h-full object-cover"
                     />
                   )}
                 </div>
               </div>
+
               <ClipPath />
             </div>
           ))}
